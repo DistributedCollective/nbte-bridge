186065b83b2d2d8b4e30b91c2c2215863ada4cbe
Rainer Koirikivi <rainer@koirikivi.fi>
2024-01-16 00:13:13 +0200

    Remove python-bitcoinlib dependency

--- bridge_node/bridge/btc/rpc.py
+++ bridge_node/bridge/btc/rpc.py
@@ -1,20 +1,20 @@
 import typing
-import bitcoin
-import bitcoin.rpc
+import bitcointx
+import bitcointx.rpc
 from anemic.ioc import Container, service
 
 from ..config import Config
 
-BitcoinRPC = typing.NewType("BitcoinRPC", bitcoin.rpc.Proxy)
+BitcoinRPC = typing.NewType("BitcoinRPC", bitcointx.rpc.RPCCaller)
 
 
 @service(interface_override=BitcoinRPC, scope="global")
 def bitcoin_rpc_factory(container: Container):
     config = container.get(interface=Config)
 
-    # NOTE: bitcoin network needs to be configured somewhere, and it's a global var
+    # NOTE: bitcoin network needs to be configured somewhere, and it's a global var (well, threadlocal)
     # let's do it here... but it also means that all bitcoin operations will fail until this factory
     # is called
-    bitcoin.SelectParams(config.btc_network)
+    bitcointx.select_chain_params("bitcoin/" + config.btc_network)
 
-    return bitcoin.rpc.Proxy(config.btc_rpc_url)
+    return bitcointx.rpc.RPCCaller(config.btc_rpc_url)
