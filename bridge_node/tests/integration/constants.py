b39ab05ac27e4e7894c7976e87778d133e70d274
Rainer Koirikivi <rainer@koirikivi.fi>
2024-01-15 17:13:23 +0200

    Partially working btclib tests

---
+++ bridge_node/tests/integration/constants.py
@@ -0,0 +1,12 @@
+MULTISIG_XPRVS = [
+    "tprv8ZgxMBicQKsPdLiVtqrvinq5JyvByQZs4xWMgzZ3YWK7ndu27yQ3qoWivh8cgdtB3bKuYKWRKhaEvtykaFCsDCB7akNdcArjgrCnFhuDjmV",
+    "tprv8ZgxMBicQKsPdMXsXv4Ddkgimo1m89QjXBNUrCgAaDRX5tEDMVA8HotnZmHcMvUVtgh1yXbN74StoJqv76jvRxJmkr2wvkPwTbZb1zeXv3Y",
+    "tprv8ZgxMBicQKsPcwXzdBoYKEXmLiBsPzNRfoLadw8WsnmKSHU47fJR3UjAhni8kt5bx5jFG9JZ4oZuxnaX6beTNwc2C5coMHmAvnKpqHA8xVb",
+]
+MULTISIG_XPUBS = [
+    "tpubD6NzVbkrYhZ4WokHnVXX8CVBt1S88jkmeG78yWbLxn7Wd89nkNDe2J8b6opP4K38mRwXf9d9VVN5uA58epPKjj584R1rnDDbk6oHUD1MoWD",
+    "tpubD6NzVbkrYhZ4WpZfRZip3ALqLpXhHUbe6UyG8iiTzVDuvNUyysyiUJWejtbszZYrDaUM8UZpjLmHyvtV7r1QQNFmTqciAz1fYSYkw28Ux6y",
+    "tpubD6NzVbkrYhZ4WQZnWqU8ieBsujhoZKZLF6wMvTApJ4ZiGmipk481DyM2su3y5BDeB9fFLwSmmmsGDGJum79he2fnuQMnpWhe3bGir7Mf4uS",
+]
+MULTISIG_KEY_DERIVATION_PATH = "m/0/0/0"
+MULTISIG_ADDRESS = "bcrt1qtxysk2megp39dnpw9va32huk5fesrlvutl0zdpc29asar4hfkrlqs2kzv5"
---
+++ bridge_node/tests/integration/test_btclib.py
@@ -0,0 +1,152 @@
+from decimal import Decimal
+import pytest
+
+try:
+    from btclib import bip32
+    from btclib import script
+    from btclib.psbt import Psbt
+    from btclib.tx import Tx, TxIn, TxOut, OutPoint
+except ImportError:
+    btclib = pytest.importorskip("btclib")
+from .constants import (
+    MULTISIG_XPUBS,
+    MULTISIG_XPRVS,
+    MULTISIG_ADDRESS,
+    MULTISIG_KEY_DERIVATION_PATH,
+)
+from .utils import from_satoshi, to_satoshi
+
+
+@pytest.fixture()
+def multisig_child_pubkeys():
+    ret = [
+        bip32.derive(master_pubkey, MULTISIG_KEY_DERIVATION_PATH)
+        for master_pubkey in MULTISIG_XPUBS
+    ]
+    ret.sort(key=lambda x: bip32.BIP32KeyData.b58decode(x).key)
+    # print([
+    #     bip32.BIP32KeyData.b58decode(x).key.hex()
+    #     for x in ret
+    # ])
+    return ret
+
+
+@pytest.fixture()
+def multisig_child_privkeys():
+    ret = [
+        bip32.derive(master_privkey, MULTISIG_KEY_DERIVATION_PATH)
+        for master_privkey in MULTISIG_XPRVS
+    ]
+    ret.sort(key=lambda x: bip32.BIP32KeyData.b58decode(bip32.xpub_from_xprv(x)).key)
+    # print([
+    #     bip32.BIP32KeyData.b58decode(
+    #         bip32.xpub_from_xprv(x)
+    #     ).key.hex()
+    #     for x in ret
+    # ])
+    return ret
+
+
+@pytest.fixture()
+def multisig_redeem_script(multisig_child_pubkeys):
+    return script.ScriptPubKey.p2ms(
+        m=2,
+        keys=multisig_child_pubkeys,
+        network="regtest",
+    )
+
+
+@pytest.fixture()
+def multisig_script(multisig_redeem_script):
+    return script.ScriptPubKey.p2wsh(
+        network="regtest",
+        redeem_script=multisig_redeem_script.script,
+    )
+
+
+def test_xprvs_match_pubkeys():
+    master_pubkeys = [bip32.xpub_from_xprv(xprv) for xprv in MULTISIG_XPRVS]
+    assert master_pubkeys == MULTISIG_XPUBS
+
+
+def test_derive_multisig_address(multisig_script, multisig_child_privkeys):
+    assert multisig_script.address == MULTISIG_ADDRESS
+
+
+def test_create_psbt(
+    multisig_bitcoin_rpc,
+    user_bitcoin_rpc,
+    multisig_script,
+    multisig_redeem_script,
+    multisig_child_privkeys,
+    multisig_child_pubkeys,
+):
+    user_btc_before = from_satoshi(user_bitcoin_rpc.getbalance())
+    print("User BTC before", user_btc_before)
+
+    utxos = multisig_bitcoin_rpc.call("listunspent", 0, 9999999, [MULTISIG_ADDRESS])
+    assert len(utxos) > 0, "Sanity check failed, no utxos to spend"
+
+    amount_btc = Decimal("0.1")
+    amount_satoshi = to_satoshi(amount_btc)
+
+    print(utxos[0])
+
+    input_utxos = [utxos[0]]
+    print(input_utxos)
+    total_amount_in_btc = sum(utxo["amount"] for utxo in input_utxos)
+    # total_amount_in_satoshi = sum(utxo['amount'] for utxo in input_utxos)
+    # total_amount_in_btc = from_satoshi(total_amount_in_satoshi)
+    fee_btc = from_satoshi(1000)
+    assert (
+        total_amount_in_btc > amount_btc + fee_btc
+    ), "Sanity check failed, not enough utxos to cover amount + fee"
+
+    change_amount_btc = total_amount_in_btc - amount_btc - fee_btc
+    change_amount_satoshi = to_satoshi(change_amount_btc)
+
+    # user_address = str(user_bitcoin_rpc.getnewaddress())
+    user_address = "bcrt1q5uypnmxn6xu4z4yvq87c5htktz2ka0dkpqtp9c"  # TODO: re-create every time!
+    user_script = script.ScriptPubKey.from_address(user_address)
+
+    input_txs_raw = [
+        multisig_bitcoin_rpc.call("gettransaction", utxo["txid"], True) for utxo in input_utxos
+    ]
+
+    tx = Tx(
+        vin=[
+            TxIn(
+                prev_out=OutPoint(
+                    tx_id=utxo["txid"],
+                    vout=utxo["vout"],
+                )
+            )
+            for utxo in input_utxos
+        ],
+        vout=[
+            TxOut(
+                value=amount_satoshi,
+                script_pub_key=user_script,
+            ),
+            TxOut(
+                value=change_amount_satoshi,
+                script_pub_key=multisig_script,
+            ),
+        ],
+    )
+    print(tx)
+    psbt = Psbt.from_tx(tx)
+    print(psbt)
+    for i, input_tx_raw in enumerate(input_txs_raw):
+        input_tx = Tx.parse(input_tx_raw["hex"])
+        psbt.inputs[i].non_witness_utxo = input_tx
+        psbt.inputs[i].redeem_script = multisig_redeem_script.script
+    psbt.assert_valid()
+    psbt.assert_signable()
+
+    # TODO: make the signatures work
+    for privkey, pubkey in zip(multisig_child_privkeys[:2], multisig_child_pubkeys):
+        for psbt_in in psbt.inputs:
+            pass
+
+    pass
