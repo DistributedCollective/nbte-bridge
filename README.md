924becf8055aa4b680d59df713807424da8b5307
Teemu Risikko <teemu.risikko@gmail.com>
2024-02-27 15:36:29 +0200

    Use named volumes and docker compose instead of docker-compose

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Install for development (requires python3.11 and poetry)
 make install
 ```
 
-Run integration tests (requires docker and docker-compose)
+Run integration tests (requires docker and docker compose)
 ```
 cat POSTGRES_PASSWORD=myrandompassword > .env
 make integration-test
--- post_docker_startup.sh
+++ post_docker_startup.sh
@@ -5,24 +5,24 @@
 
 set -e
 echo "Mining initial block"
-docker-compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 1 bcrt1qtxysk2megp39dnpw9va32huk5fesrlvutl0zdpc29asar4hfkrlqs2kzv5
+docker compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 1 bcrt1qtxysk2megp39dnpw9va32huk5fesrlvutl0zdpc29asar4hfkrlqs2kzv5
 sleep 2
 ALICE_ADDR=$(
-    docker-compose exec -u lnd alice-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
+    docker compose exec -u lnd alice-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
 )
 echo "Generating 101 blocks to Alice: $ALICE_ADDR"
-docker-compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $ALICE_ADDR
+docker compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $ALICE_ADDR
 
 BOB_ADDR=$(
-    docker-compose exec -u lnd bob-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
+    docker compose exec -u lnd bob-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
 )
 echo "Generating 101 blocks to Bob: $BOB_ADDR"
-docker-compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $BOB_ADDR
+docker compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $BOB_ADDR
 
 USER_ADDR=$(
-    docker-compose exec -u lnd user-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
+    docker compose exec -u lnd user-lnd /opt/lnd/lncli -n regtest newaddress p2tr | jq -r .address
 )
 echo "Generating 101 blocks to User: $USER_ADDR"
-docker-compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $USER_ADDR
+docker compose exec bitcoind bitcoin-cli -datadir=/home/bitcoin/.bitcoin -regtest generatetoaddress 101 $USER_ADDR
 
 echo "All done!"
