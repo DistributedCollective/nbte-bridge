0c88bbcc51df6a4d3320e64fc48dece41c52e8f1
longnguyen <long.nguyendr@gmail.com>
2024-02-29 17:04:22 +0200

    Update ethereum hook: fetch token balances of the current user from hardcoded token addresses.

--- bridge_frontend/src/stores/useEthereumStore.tsx
+++ bridge_frontend/src/stores/useEthereumStore.tsx
@@ -1,11 +1,20 @@
 import {create, createStore} from "zustand";
 import {ethers} from "ethers";
+import axios from "axios";
+
+interface TokenBalance {
+  symbol: string;
+  balance: string;
+  name: string;
+  tokenContractAddress: string;
+}
 
 interface EthereumState {
   provider: ethers.BrowserProvider | null;
   signer: ethers.Signer | null;
   account: string | null;
   balance: string | null;
+  tokenBalances: Array<TokenBalance>;
   chainId: string | null;
   contract: ethers.Contract | null;
   isMetaMaskInstalled: boolean;
@@ -26,6 +35,7 @@ const ethereumStore = create<EthereumState>((set) => ({
   signer: null,
   account: null,
   balance: null,
+  tokenBalances: [],
   chainId: null,
   contract: null,
   newBlockSubscriptionId: [],
@@ -40,6 +50,8 @@ const ethereumStore = create<EthereumState>((set) => ({
       try {
         const accounts = await ethereum.request({method: 'eth_requestAccounts'});
         const chainId = await ethereum.request({method: 'eth_chainId'});
+        const parsedChainId = ethers.formatUnits(chainId, 0);
+
         const webProvider = new ethers.BrowserProvider(ethereum);
         const webSigner = await webProvider.getSigner();
         const balance = await ethereum.request({
@@ -47,14 +59,35 @@ const ethereumStore = create<EthereumState>((set) => ({
           params: [accounts[0], 'latest']
         });
         console.log("signer: ", webSigner);
+
+        const tokenAddresses = ["0x5FbDB2315678afecb367f032d93F642f64180aa3", "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"]
+        const tokenBalances: Array<TokenBalance> = [];
+        for (const address of tokenAddresses) {
+          const tokenABI = [
+            "function balanceOf(address) view returns (uint)",
+            "function symbol() view returns (string)",
+            "function name() view returns (string)"
+          ]
+          const tokenContract = new ethers.Contract(address, tokenABI, webProvider);
+          const balance = await tokenContract.balanceOf(accounts[0]);
+          const symbol = await tokenContract.symbol();
+          const name = await tokenContract.name();
+          tokenBalances.push({
+            symbol: symbol,
+            balance: ethers.formatEther(balance),
+            name: name,
+            tokenContractAddress: address
+          });
+        }
         set((
           state: EthereumState
         ) => ({
           provider: webProvider,
           signer: webSigner,
           account: accounts[0],
-          chainId: chainId,
+          chainId: parsedChainId,
           balance: ethers.formatEther(balance),
+          tokenBalances: tokenBalances,
         }));
       } catch (e) {
         console.error(e)
