d4071c6b6f3a71291e8de2f244a1ca15d93f9935
longnguyen <long.nguyendr@gmail.com>
2024-02-29 17:02:21 +0200

    Move connect metamask button to navbar and replace html components with react-bootstrap.

--- bridge_frontend/src/components/TopNav.tsx
+++ bridge_frontend/src/components/TopNav.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import useEthereumStore from "../stores/useEthereumStore";
+import {useStore} from "zustand";
+import ethereumStore from "../stores/useEthereumStore";
+import {Nav, Navbar} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const TopNav = () => {
-  const {account} = useEthereumStore();
+  const {account, chainId} = useEthereumStore();
 
   return (
-    <nav style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '20px',
-      backgroundColor: 'lightgray',
-      marginBottom: '20px',
-    }}>
-      <div>
-        <h2>Rune Brigde DApp</h2>
-      </div>
-      <div>
-        {
-          account && <p>{account}</p>
-        }
-      </div>
-    </nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand>
+          <h2>Rune Brigde DApp</h2>
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          {
+            account ? (
+                <Navbar.Text>
+                  <p>{account}</p>
+                  <p>ChainId: {chainId}</p>
+                </Navbar.Text>
+            ) : (
+              <MetamaskConnectButton/>
+            )
+          }
+        </Navbar.Collapse>
+
+
+      </Container>
+    </Navbar>
   );
 };
 
 export default TopNav;
+
+export const MetamaskConnectButton = () => {
+  const {connectMetamask} = useStore(ethereumStore);
+  return (
+    <Button className="btn btn-primary" onClick={connectMetamask}>Connect Metamask</Button>
+  );
+}
