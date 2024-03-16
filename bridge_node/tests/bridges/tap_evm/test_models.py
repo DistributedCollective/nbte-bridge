26dec3ca363f08f27bcc2376886c3fe70bf27cfb
Rainer Koirikivi <rainer@koirikivi.fi>
2024-03-16 12:49:01 +0200

    Fix test_models test

--- bridge_node/tests/bridges/tap_evm/test_models.py
+++ bridge_node/tests/bridges/tap_evm/test_models.py
@@ -1,7 +1,6 @@
 import pytest
-from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
-from bridge.common.models.meta import Base
+
 from bridge.bridges.tap_rsk.models import (
     RskToTapTransferBatch,
     RskToTapTransferBatchStatus,
@@ -9,22 +8,10 @@ from bridge.bridges.tap_rsk.models import (
     TapToRskTransferBatchStatus,
 )
 
-DEV_DB_NAME = "nbte_tmp_test"
-
-
-@pytest.fixture(scope="session")
-def engine(postgres):
-    postgres.cli(f"DROP DATABASE IF EXISTS {DEV_DB_NAME};")
-    postgres.cli(f"CREATE DATABASE {DEV_DB_NAME};")
-
-    engine = create_engine(f"postgresql://postgres:foo@localhost:65432/{DEV_DB_NAME}", echo=False)
-    Base.metadata.create_all(engine)
-    return engine
-
 
 @pytest.fixture
-def dbsession(engine):
-    session = Session(bind=engine)
+def dbsession(dbengine):
+    session = Session(bind=dbengine)
     yield session
     session.rollback()
 
