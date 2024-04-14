3501c6397e49fc832eb2c27ef6626a06e4c70bf2
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-14 12:03:22 +0300

    Remove unused import

--- bridge_contracts/test/runes/RuneToken.test.ts
+++ bridge_contracts/test/runes/RuneToken.test.ts
@@ -1,7 +1,6 @@
 import {loadFixture} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import {ethers} from "hardhat";
 import {expect} from 'chai';
-import {RuneToken} from '../../typechain-types';
 
 
 interface DeployArgs {
