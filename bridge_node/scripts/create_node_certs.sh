e3caa63388112684be470ba807974ad4c2c255ed
Rainer Koirikivi <rainer@koirikivi.fi>
2024-04-19 23:07:33 +0300

    Fix IP certs, openssl bug

--- bridge_node/scripts/create_node_certs.sh
+++ bridge_node/scripts/create_node_certs.sh
@@ -6,12 +6,12 @@ NODE_NAME=$BRIDGE_HOSTNAME
 openssl req -new -nodes -newkey ec -pkeyopt ec_paramgen_curve:P-256 -out server.csr -keyout server-key.pem -subj "/O=Sovryn/CN=$(hostname)" -addext "subjectAltName = IP:${NODE_NAME}" -addext "extendedKeyUsage = serverAuth"
 
 # Sign the node server CSR with the CA certificate
-openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -days 365
+openssl x509 -req -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -days 365 -copy_extensions=copyall
 
 # Create node client key and certificate signing request (CSR)
 openssl req -new -nodes -newkey ec -pkeyopt ec_paramgen_curve:P-256 -out client.csr -keyout client-key.pem -subj "/O=Sovryn/CN=$(hostname)" -addext "subjectAltName = IP:${NODE_NAME}" -addext "extendedKeyUsage = clientAuth"
 
 # Sign the node client CSR with the CA certificate
-openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 365
+openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 365 -copy_extensions=copyall
 
 chmod 600 *.pem
