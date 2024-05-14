45dc0abda593808778f3a1b4fb05ddde2fb3af8b
Rainer Koirikivi <rainer@koirikivi.fi>
2024-05-14 13:50:42 +0300

    Ignore Pyro5 address recovery errors in sentry

--- bridge_node/bridge/sentry.py
+++ bridge_node/bridge/sentry.py
@@ -2,6 +2,7 @@ import logging
 import warnings
 
 import sentry_sdk
+from Pyro5.errors import CommunicationError, SecurityError
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,21 @@ def init_sentry(dsn):
         warnings.warn("Sentry DSN not set, Sentry disabled", stacklevel=2)
         return
 
+    def before_send(event, hint):
+        if "exc_info" in hint:
+            exc_value = hint["exc_info"][1]
+
+            # TODO: Address recovered from Pyro5 handshake is sometimes not valid. Not sure why this would happen --
+            #       it needs to be investigated, but for now let's just ignore
+            if isinstance(exc_value, CommunicationError | SecurityError):
+                if "does not match any of the allowed addresses" in str(exc_value):
+                    logger.info("Ignoring Pyro5 error: %s", exc_value)
+                    return None
+
+        return event
+
     logger.info("Initializing Sentry")
     sentry_sdk.init(
         dsn=dsn,
+        before_send=before_send,
     )
