e0d5190d4bd0ae8b3c4296f1de16373a3a71594a
Rainer Koirikivi <rainer@koirikivi.fi>
2024-08-02 16:40:33 +0300

    Change license to GPLv3 (or later)

--- bridge_contracts/docker-minimal/contracts/MinimalContract.sol
+++ bridge_contracts/docker-minimal/contracts/MinimalContract.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later-or-later
 // This file is here to speed up the docker build (it allows it to download the solidity version in a previous
 // stage). Keep the solidity version in line with what's actually used.
 pragma solidity ^0.8.9;
